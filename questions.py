e4328df8fb54a1d8944115e05fb1ab82e876afa4
DE0414\User <aslandukov-matvej@yandex.ua>
2020-04-06 14:55:28 +0300

    Change questions

--- questions.py
+++ questions.py
@@ -3,43 +3,23 @@ from utils import get_str_value, round_to
 
 
 def get_question_by_id(id):
-    if id == 1:
+    if id <= 2:
         a = np.random.randint(1, 51)
         b = np.random.randint(1, 51)
-        tp = np.random.randint(7)
-        if tp == 0:
-            res = '{} - {}'.format(a, b)
-        elif tp == 1:
-            res = '-{} - {}'.format(a, b)
-        elif tp == 2:
-            res = '-{} + {}'.format(a, b)
-        elif tp == 3:
-            res = '-{} + (-{})'.format(a, b)
-        elif tp == 4:
-            res = '{} + (-{})'.format(a, b)
-        elif tp == 5:
-            res = '-{} - (-{})'.format(a, b)
-        elif tp == 6:
-            res = '{} - (-{})'.format(a, b)
-        return 'Вычисли: {}'.format(res), eval(res)
-    elif id == 2:
-        a = np.random.randint(1, 200) / 10
-        b = np.random.randint(1, 200) / 10
-        tp = np.random.randint(5)
-        if tp == 0:
-            res = '{} - {}'.format(get_str_value(a), get_str_value(b))
-        elif tp == 1:
-            res = '-{} - {}'.format(get_str_value(a), get_str_value(b))
-        elif tp == 2:
-            res = '-{} + {}'.format(get_str_value(a), get_str_value(b))
-        elif tp == 3:
-            res = '-{} + (-{})'.format(get_str_value(a), get_str_value(b))
-        elif tp == 4:
-            res = '{} + (-{})'.format(get_str_value(a), get_str_value(b))
-        elif tp == 5:
-            res = '-{} - (-{})'.format(get_str_value(a), get_str_value(b))
-        elif tp == 6:
-            res = '{} - (-{})'.format(get_str_value(a), get_str_value(b))
+        if id == 2:
+            a = get_str_value(np.random.randint(1, 100) / 10)
+            b = get_str_value(np.random.randint(1, 100) / 10)
+        all = ['{} - {}'.format(a, b),
+               '-{} - {}'.format(a, b),
+               '-{} + {}'.format(a, b),
+               '-{} + (-{})'.format(a, b),
+               '{} + (-{})'.format(a, b),
+               '-{} - (-{})'.format(a, b),
+               '{} - (-{})'.format(a, b),
+               '-{} - (-{})'.format(a, b),
+               '-(-{}) + {}'.format(a, b),
+               '-(-{}) - {}'.format(a, b)]
+        res = np.random.choice(all, 1)[0]
         return 'Вычисли: {}'.format(res), eval(res)
     
     
